335b298db9d8e629294f82711b71d00c5e8bb023
KonJengz <gecko.jeng@gmail.com>
2025-03-29 17:35:55 +0700

    update login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import Button from '../components/Button'
+import React, { useState } from "react";
+import Button from "../components/Button";
+import Welcome from "../icons/Welcome";
 
 function LoginPage() {
-    return (
-        <div className='flex justify-center items-center min-h-screen w-full h-full gap-3 text-4xl'>
-            <div className='text-8xl flex flex-col justify-center text-center flex-1/2 gap-5'>
-                <h1>"</h1>
-                <h1>WELCOME</h1>
-                <h1>TO BEING</h1>
-                <h1>DEVELOPER!</h1>
-                <h1>"</h1>
-            </div>
-            <div className=' flex flex-col justify-between  gap-10 flex-1/2'>
-                <div>
-                    <h1>Let's Get Started</h1>
-                </div>
-                <div>
+  const [input, setInput] = useState();
+  return (
+    <div className="flex justify-center items-center min-h-screen gap-32">
+      <div className="">
+        <Welcome className="w-96" />
+      </div>
 
-                    <input type="text" placeholder='Username' className='border-b-2 border-y-teal-400' />
-                </div>
-                <div>
-
-                    <input type="password" placeholder='Password' className='border-b-2 border-y-teal-400' />
-                </div>
-                <div className='flex gap-2'>
-                    <input type="checkbox" />
-                    <h1 className='text-3xl'>I promise that I will try to do it by myself without using AI.</h1>
-                </div>
-                <div>
-                    <Button />
-                </div>
-            </div>
+      <div className="w-96">
+        <h1 className="text-2xl">Let's Get Started</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            className="border-b-2 border-y-teal-400"
+          />
         </div>
-    )
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            className="border-b-2 border-y-teal-400"
+          />
+        </div>
+        <div className="flex gap-2">
+          <input type="checkbox" />
+          <h1 className="text-xs">
+            I promise that I will try to do it by myself without using AI.
+          </h1>
+        </div>
+        <div>
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
